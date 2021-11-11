a78c15405202461b263f0903449531abd35feba2
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2021-11-11 10:07:02 +0300

    caps generated

--- Hafta4/caps-app/src/App.js
+++ Hafta4/caps-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import AppContainer from './Components/AppContainer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SingleMeme from './Components/SingleMeme';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppContainer />} />
+          <Route path="/single-caps/:id" element={<SingleMeme />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
